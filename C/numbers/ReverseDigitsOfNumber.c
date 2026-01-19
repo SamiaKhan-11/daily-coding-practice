d25ac70c48a14451c4b59be9d25614d70b085a4e
samia26khan@gmail.com <samia26khan@gmail.com>
2026-01-19 23:23:34 +0530

    Added a program to reverse the digits of a number.

---
+++ C/numbers/ReverseDigitsOfNumber.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int num,reverse=0,d,temp;
+    printf("Enter a number : ");
+    scanf("%d",&num);
+    temp=num;
+    while(num>0){
+        d=num%10;
+        num=num/10;
+        reverse=reverse*10+d;
+    }
+    printf("Reverse of the number %d is %d",temp,reverse);
+    return 0;
+}
