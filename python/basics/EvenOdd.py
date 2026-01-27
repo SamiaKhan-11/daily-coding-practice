2e375343221990cd0a288da959c3b0b7634cca8d
samia26khan@gmail.com <samia26khan@gmail.com>
2026-01-28 00:09:35 +0530

    Added basic python programs

---
+++ python/basics/EvenOdd.py
@@ -0,0 +1,21 @@
+a=int(input("Enter a number : "))
+# b=int(input("Enter a number : "))
+# sum=a+b
+# print(sum)
+
+# if a>b:
+#   print("a is greater : ",a)
+# else:
+#   print("b is greater : ",b)
+
+# for i in range(11):
+#     print (i)
+
+i=1
+fact=1
+while a>0:
+    fact=fact*a
+    a-=1
+print("Factorial is : ",fact)
+
+
